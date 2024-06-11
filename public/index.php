5b5d3b9ddfdeaad70bcefdd5ac840a6cb389a77f
Kimberley Bordier <bdrkim41600@gmail.com>
2024-06-11 12:29:39 +0200

    a

--- public/index.php
+++ public/index.php
@@ -1,14 +1,35 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// header("Access-Control-Allow-Origin: http://localhost:3000");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-header("Content-Type: application/json");
+// header("Content-Type: application/json");
 
-$artworks = [
-    ["id" => 1, "title" => "Artwork 1", "description" => "Description of artwork 1"],
-    ["id" => 2, "title" => "Artwork 2", "description" => "Description of artwork 2"],
-    // Ajoutez plus d'œuvres ici
-];
+require_once '../src/controllers/UserController.php';
 
-echo json_encode($artworks);
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$controller = new UserController();
+
+$action = isset($_GET['action']) ? $_GET['action'] : die(json_encode(["message" => "No action specified."]));
+
+switch ($action) {
+    case 'createUser':
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = json_decode(file_get_contents("php://input"));
+            echo $controller->createUser($data->username, $data->email, $data->password);
+        }
+        break;
+
+    case 'getAllUsers':
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            echo $controller->getAllUsers();
+        }
+        break;
+
+    default:
+        echo json_encode(["message" => "Invalid action."]);
+        break;
+}
+?>
---
+++ src/controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+require_once '../models/User.php';
+require_once '../config/database.php';
+
+class UserController {
+    private $db;
+    private $user;
+
+    public function __construct() {
+        $database = new Database();
+        $this->db = $database->getConnection();
+        $this->user = new User($this->db);
+    }
+
+    // Fonction pour créer un utilisateur
+    public function createUser($username, $email, $password) {
+        $this->user->username = $username;
+        $this->user->email = $email;
+        $this->user->password = password_hash($password, PASSWORD_BCRYPT);
+
+        if ($this->user->create()) {
+            return json_encode(["message" => "User created successfully."]);
+        } else {
+            return json_encode(["message" => "User could not be created."]);
+        }
+    }
+
+    // Fonction pour obtenir tous les utilisateurs
+    public function getAllUsers() {
+        $stmt = $this->user->readAll();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return json_encode($users);
+    }
+}
+?>
